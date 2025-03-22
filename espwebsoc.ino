8a0e2c3f7d291e06d5e5ca3a48da481705512de5
zidantech <zidantech4@gmail.com>
2025-03-22 15:52:05 +0100

    2

--- espwebsoc.ino
+++ espwebsoc.ino
@@ -2,6 +2,25 @@
 #include <WiFi.h>
 #include <WebSocketsClient.h>
 
+// Camera pin definitions for ESP32-CAM
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
 const char* ssid = "UAS_CONNECT";
 const char* password = "uas@vtol";
 const char* websocketServer = "wss://esp32-cam-kpjw.onrender.com";
@@ -31,6 +50,7 @@ void configCamera() {
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
 
+  // Reduce resolution and quality for faster transmission
   config.frame_size = FRAMESIZE_QVGA; // 320x240
   config.jpeg_quality = 10; // Lower quality for smaller file size
   config.fb_count = 1;
