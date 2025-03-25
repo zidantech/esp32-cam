ee4a40441fb840e889d1585bd38a6482a1f505bc
zidantech <zidantech4@gmail.com>
2025-03-25 14:55:30 +0100

    port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 
 // Serve static frontend files
 app.use(express.static(path.join(__dirname, 'public')));
